30a9c7d9cbe169f4a56e721ed395931850ab0351
Julian Kropff <j.kropff@stud.unibas.ch>
2026-04-24 09:04:12 +0200

    go arch

---
+++ .config/nvim/lua/plugins/go.lua
@@ -0,0 +1,39 @@
+-- plugins/go.lua
+
+return {
+--   {
+--     "ray-x/go.nvim",
+--     dependencies = {
+--       "ray-x/guihua.lua",
+--       "neovim/nvim-lspconfig",
+--       "nvim-treesitter/nvim-treesitter",
+--     },
+--
+--     event = { "CmdlineEnter" },
+--     ft = { "go", "gomod" },
+--
+--     build = function()
+--       require("go.install").update_all_sync()
+--     end,
+--
+--     opts = function(_, opts)
+--       require("go").setup(opts)
+--
+--       local format_sync_grp =
+--         vim.api.nvim_create_augroup("GoFormat", { clear = true })
+--
+--       vim.api.nvim_create_autocmd("BufWritePre", {
+--         pattern = "*.go",
+--         group = format_sync_grp,
+--         callback = function()
+--           require("go.format").goimports()
+--         end,
+--       })
+--
+--       return {
+--         -- lsp_keymaps = false,
+--         -- weitere Optionen hier
+--       }
+--     end,
+--   },
+}
