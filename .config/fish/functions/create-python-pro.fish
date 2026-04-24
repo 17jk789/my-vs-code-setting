73f0a252e5caae215c04628c181efcbf0654f54a
Julian Kropff <j.kropff@stud.unibas.ch>
2026-04-24 17:58:53 +0200

    fix fish

--- .config/fish/functions/create-python-pro.fish
+++ .config/fish/functions/create-python-pro.fish
@@ -1,4 +1,4 @@
-function create-python-pro --argument-symbol action name
+function create-python-pro --argument-names action name
     # Prüfen auf korrekte Argumente
     if test "$action" != "new"; or test -z "$name"
         echo "Usage: create-python-pro new <projektname>"
