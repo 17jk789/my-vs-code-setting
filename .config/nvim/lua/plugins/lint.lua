1548c5d5a2aa107ee2f152c3d6422877f307b820
Julian Kropff <j.kropff@stud.unibas.ch>
2026-05-27 16:05:21 +0200

    fix neovim arch config

---
+++ .config/nvim/lua/plugins/lint.lua
@@ -0,0 +1,18 @@
+-- plugins/lint.lua
+
+return {
+  "mfussenegger/nvim-lint",
+  config = function()
+    local lint = require("lint")
+
+    lint.linters_by_ft = {
+      python = { "ruff", "mypy", "bandit" },
+    }
+
+    vim.api.nvim_create_autocmd({ "BufWritePost", "BufEnter" }, {
+      callback = function()
+        lint.try_lint()
+      end,
+    })
+  end,
+}
