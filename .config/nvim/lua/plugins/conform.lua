f4d5dd3f7b1e0a6a6f3a812fd0036fcc9be38c09
Julian Kropff <j.kropff@stud.unibas.ch>
2026-05-27 16:05:26 +0200

    fix neovim arch config

---
+++ .config/nvim/lua/plugins/conform.lua
@@ -0,0 +1,14 @@
+-- plugins/conform.lua
+
+return {
+  "stevearc/conform.nvim",
+  opts = {
+    formatters_by_ft = {
+      python = { "black" },
+    },
+
+    format_on_save = {
+      timeout_ms = 2000,
+    },
+  },
+}
