e1352bccf2ae4592a65343a7cab096850fa74de0
Julian Kropff <j.kropff@stud.unibas.ch>
2026-05-27 14:41:26 +0200

    fix

--- .config/nvim/lua/lsp/docker.lua
+++ .config/nvim/lua/lsp/docker.lua
@@ -2,31 +2,21 @@
 
 local M = {}
 
--- Registriert den Autocommand für Dockerfiles
-vim.api.nvim_create_autocmd("FileType", {
-    pattern = "dockerfile",
-    callback = function()
-        local lspconfig = require('lspconfig')
+M.setup = function(capabilities)
+    local lspconfig = require("lspconfig")
 
-        -- Standard-Capabilities vorbereiten
-        local capabilities = vim.lsp.protocol.make_client_capabilities()
-        local status_cmp, cmp_nvim_lsp = pcall(require, 'cmp_nvim_lsp')
-        if status_cmp then
-            capabilities = cmp_nvim_lsp.default_capabilities(capabilities)
-        end
+    lspconfig.dockerls.setup({
+        capabilities = capabilities,
+        filetypes = { "dockerfile" },
 
-        lspconfig.dockerls.setup({
-            capabilities = capabilities,
-            on_attach = function(client, bufnr)
-                -- Hilfsfunktion für Keymaps (falls du bufferspezifische Keys brauchst)
-                local buf_map = function(mode, lhs, rhs, opts)
-                    opts = opts or {}
-                    opts.buffer = bufnr
-                    vim.keymap.set(mode, lhs, rhs, opts)
-                end
-            end,
-        })
-    end,
-})
+        on_attach = function(client, bufnr)
+            local buf_map = function(mode, lhs, rhs, opts)
+                opts = opts or {}
+                opts.buffer = bufnr
+                vim.keymap.set(mode, lhs, rhs, opts)
+            end
+        end,
+    })
+end
 
 return M
