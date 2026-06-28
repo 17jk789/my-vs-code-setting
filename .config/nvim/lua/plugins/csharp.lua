2a02f3929883124d462ecd59c9ee0d9eb5f08c8e
Julian Kropff <j.kropff@stud.unibas.ch>
2026-06-28 20:11:41 +0200

    fix

--- .config/nvim/lua/plugins/csharp.lua
+++ .config/nvim/lua/plugins/csharp.lua
@@ -6,16 +6,8 @@ return {
     "seblj/roslyn.nvim",
     ft = { "cs", "razor", "cshtml" },
 
-    dependencies = {
-      "hrsh7th/cmp-nvim-lsp",
-    },
-
     config = function()
-      local capabilities = vim.tbl_deep_extend(
-        "force",
-        vim.lsp.protocol.make_client_capabilities(),
-        require("cmp_nvim_lsp").default_capabilities()
-      )
+      local capabilities = require("blink.cmp").get_lsp_capabilities()
 
       require("roslyn").setup({
         args = {
---
+++ .config/nvim/lua/plugins/disable-cmp.lua
@@ -0,0 +1,6 @@
+-- plugins/disable-cmp.lua
+
+return {
+  { "hrsh7th/nvim-cmp", enabled = false },
+  { "hrsh7th/cmp-nvim-lsp", enabled = false },
+}
